c14fa40a1f223d434fce7ded12182b314f363000
Carlos Pérez Alzina <carlosperezalzina@gmail.com>
2025-05-20 18:33:36 +0200

    Traducción al español ampliada

--- fix_bom.ps1
+++
@@ -1,5 +0,0 @@
-$filePath = ".\localisation\english\alcoy_focus_l_english.yml"
-$content = Get-Content -Path $filePath -Raw
-$utf8BomEncoding = New-Object System.Text.UTF8Encoding($true)
-[System.IO.File]::WriteAllText($filePath, $content, $utf8BomEncoding)
-Write-Host "Archivo convertido a UTF-8 con BOM: $filePath"
